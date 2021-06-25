e95c5f63450df4ab077829c435f8ae51ea418d94
Elia Perantoni <perantonielia0@gmail.com>
2021-06-25 13:44:55 +0200

    Now using `assimp` crate instead of `russimp`

--- src/model.rs
+++ src/model.rs
@@ -1,10 +1,26 @@
+use std::borrow::Borrow;
 use std::cell::RefCell;
 use std::error::Error;
 use std::mem;
 
-use russimp::mesh::Mesh;
-use russimp::node::Node;
-use russimp::scene::{PostProcess, Scene};
+use glam::{Vec2, Vec3};
+
+mod assimp {
+    pub use assimp::import::Importer;
+    pub use assimp::math::Vector3D;
+    pub use assimp::scene::{Mesh, Node, Scene};
+}
+
+pub struct Vertex {
+    position: Vec3,
+    normal: Vec3,
+    tex_coords: Vec2,
+}
+
+pub struct Mesh {
+    vertices: Vec<Vertex>,
+    indices: Vec<u32>,
+}
 
 pub struct Model {
     meshes: Vec<Mesh>,
@@ -14,21 +30,19 @@ impl Model {
     pub fn from_file(path: &str) -> Result<Model, Box<dyn Error>> {
         let mut meshes = Vec::new();
 
-        let mut scene = Scene::from_file(path, vec![
-            PostProcess::Triangulate,
-            PostProcess::FlipUVs,
-        ])?;
+        let mut importer = assimp::Importer::new();
+        importer.flip_uvs(true);
+        importer.triangulate(true);
 
-        // TODO Error out on incomplete scene
+        let mut scene = importer.read_file(path)?;
 
-        let root = match &scene.root {
-            Some(root) => RefCell::borrow(root),
-            None => return Err(Box::from(
-                format!("model at {} has no root", path)
-            ))
-        };
+        if scene.is_incomplete() {
+            return Err(Box::from(
+                format!("model at {} is incomplete", path)
+            ));
+        }
 
-        process_node(&root, &mut scene.meshes, &mut meshes);
+        process_node(&scene.root_node(), &scene, &mut meshes);
 
         Ok(Model {
             meshes,
@@ -36,16 +50,39 @@ impl Model {
     }
 }
 
-fn process_node(node: &Node, scene_meshes: &mut Vec<Mesh>, meshes: &mut Vec<Mesh>) {
-    for mesh_idx in &node.meshes {
-        let mut mesh = Mesh::default();
-        mem::swap(&mut scene_meshes[*mesh_idx as usize], &mut mesh);
-
-        meshes.push(mesh);
+fn process_node(node: &assimp::Node, scene: &assimp::Scene, meshes: &mut Vec<Mesh>) {
+    for mesh_idx in node.meshes() {
+        let mesh = &scene.mesh(*mesh_idx as _).expect(&format!("mesh with id {} not found in scene", mesh_idx));
+        meshes.push(process_mesh(mesh, scene));
     }
 
-    for child in &node.children {
-        let child = RefCell::borrow(child);
-        process_node(&child, scene_meshes, meshes);
+    for child in node.child_iter() {
+        process_node(&child, scene, meshes);
+    }
+}
+
+fn process_mesh(mesh: &assimp::Mesh, scene: &assimp::Scene) -> Mesh {
+    let vertices: Vec<_> = mesh
+        .vertex_iter()
+        .zip(mesh.normal_iter())
+        .zip(mesh.texture_coords_iter(0))
+        .map(|((position, normal), tex_coords)| {
+            Vertex {
+                position: Vec3::new(position.x, position.y, position.z),
+                normal: Vec3::new(normal.x, normal.y, normal.z),
+                tex_coords: Vec2::new(tex_coords.x, tex_coords.y),
+            }
+        })
+        .collect();
+
+    let indices: Vec<_> = mesh.face_iter().flat_map(|face| {
+        // Should always be true because we told Assimp to triangulate
+        assert_eq!(face.num_indices, 3);
+        (0..face.num_indices).map(move |i| face[i as _])
+    }).collect();
+
+    Mesh {
+        vertices,
+        indices,
     }
 }
