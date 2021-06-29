80e3037ce4d9a403d29c7e41f8023ad65092fac4
Elia Perantoni <perantonielia0@gmail.com>
2021-06-29 20:41:30 +0200

    Expose components' fields again

--- src/component.rs
+++ src/component.rs
@@ -4,13 +4,13 @@ use crate::mesh::Mesh;
 #[derive(Debug)]
 pub struct MeshComponent {
     // TODO Should probably use references instead of owned types
-    mesh: Mesh,
-    material: Material,
+    pub mesh: Mesh,
+    pub material: Material,
 }
 
 #[derive(Debug)]
 pub struct CameraComponent {
-    fov: f64,
+    pub fov: f64,
 }
 
 #[derive(Debug)]
