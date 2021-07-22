c4fbf010f0136c6f6ff05f261f4446427ceea8cd
Elia Perantoni <perantonielia0@gmail.com>
2021-07-22 17:37:39 +0200

    Yay paging!

--- raven_ecs/src/lib.rs
+++ raven_ecs/src/lib.rs
@@ -9,107 +9,122 @@ struct Entity {
     version: Version,
 }
 
-struct Pool<T: 'static> {
-    sparse: Vec<Option<usize>>,
-    packed: Vec<ID>,
-    components: Vec<T>,
-}
-
-impl<T: 'static> Pool<T> {
-    fn new() -> Self {
-        Self {
-            sparse: Vec::new(),
-            packed: Vec::new(),
-            components: Vec::new(),
-        }
-    }
-
-    fn add(&mut self, entity_id: ID, component: T) {
-        if self.sparse.len() <= entity_id {
-            self.sparse.resize(entity_id + 1, None);
-        }
-        self.sparse[entity_id] = Some(self.packed.len());
-
-        self.packed.push(entity_id);
-        self.components.push(component);
-    }
-
-    fn remove(&mut self, entity_id: ID) {
-        let packed_idx = if let Some(packed_idx) = self.sparse[entity_id] {
-            packed_idx
-        } else {
-            // This entity does not have a component. This is a NOOP
-            return;
-        };
-
-        // This is how we remove the component from the packed entity array and the packed components list:
-        //   1) Swap the last entity/component with the entity/component that we are deleting
-        //   2) Pop from both arrays
-        // But doing that, the last entity/component is swapped around and the corresponding index in the sparse array
-        // is no longer valid.
-        // This is where we fix that by settings its index to the place that it will take: the one of the component
-        // that is going to be deleted
-        self.sparse[*self.packed.last().unwrap()] = Some(packed_idx);
-
-        let last_packed_idx = self.packed.len() - 1;
-
-        self.packed.swap(packed_idx, last_packed_idx);
-        self.packed.pop();
-
-        self.components.swap(packed_idx, last_packed_idx);
-        self.components.pop();
-
-        self.sparse[entity_id] = None;
-        while matches!(self.sparse.last(), Some(None)) {
-            self.sparse.pop();
-        }
-    }
-
-    fn get(&self, entity_id: ID) -> Option<&T> {
-        self.sparse
-            .get(entity_id)
-            .copied()
-            .and_then(move |packed_idx| Some(&self.components[packed_idx?]))
-    }
-
-    fn get_mut(&mut self, entity_id: ID) -> Option<&mut T> {
-        self.sparse
-            .get(entity_id)
-            .copied()
-            .and_then(move |packed_idx| Some(&mut self.components[packed_idx?]))
-    }
-}
-
-#[cfg(test)]
-mod test {
+mod pool {
     use super::*;
 
-    #[test]
-    fn resizing() {
-        let mut p: Pool<i32> = Pool::new();
+    const PAGE_SIZE: usize = 100;
+    /// A `Page` is either null or a pointer to an array of optional indices
+    type Page = Option<Box<[Option<usize>; PAGE_SIZE]>>;
 
-        let sparse_size = |p: &Pool<_>| {
-            p.sparse.len()
-        };
+    struct Pool<T: 'static> {
+        sparse: Vec<Page>,
+        packed: Vec<ID>,
+        components: Vec<T>,
+    }
 
-        let packed_size = |p: &Pool<_>| {
-            assert_eq!(p.packed.len(), p.components.len());
-            p.packed.len()
-        };
+    impl<T: 'static> Pool<T> {
+        fn new() -> Self {
+            Self {
+                sparse: Vec::new(),
+                packed: Vec::new(),
+                components: Vec::new(),
+            }
+        }
 
-        assert_eq!(sparse_size(&p), 0);
-        assert_eq!(packed_size(&p), 0);
-        p.add(1, 1);
-        assert_eq!(sparse_size(&p), 2);
-        assert_eq!(packed_size(&p), 1);
-        p.add(2, 2);
-        assert_eq!(sparse_size(&p), 3);
-        assert_eq!(packed_size(&p), 2);
-        p.remove(1);
-        assert_eq!(sparse_size(&p), 3);
-        assert_eq!(packed_size(&p), 1);
-        p.remove(2);
-        assert_eq!(sparse_size(&p), 0);
-        assert_eq!(packed_size(&p), 0);
+        /// Index of the page where the entity with the given ID should reside
+        fn idx_to_page(entity_id: ID) -> usize {
+            entity_id / PAGE_SIZE
+        }
+
+        /// Index into the page where the entity with the given ID should reside
+        fn idx_into_page(entity_id: ID) -> usize {
+            entity_id % PAGE_SIZE
+        }
+
+        fn add(&mut self, entity_id: ID, component: T) {
+            let idx_to_page = Self::idx_to_page(entity_id);
+            let idx_into_page = Self::idx_into_page(entity_id);
+
+            // Is the vector large enough for us to be able to place the page at the correct index?
+            if self.sparse.len() <= idx_to_page {
+                // If not, make it bigger
+                self.sparse.resize(idx_to_page + 1, None);
+            }
+
+            // Get a mut ref to the page (which is just a pointer to an array or null). If it is null, insert an empty page
+            let mut page = self.sparse[idx_to_page].get_or_insert(Box::new([None; PAGE_SIZE]));
+
+            // The new component will be pushed to the end of the packed arrays so that's the index that we should store
+            page[idx_into_page] = Some(self.packed.len());
+
+            self.packed.push(entity_id);
+            self.components.push(component);
+        }
+
+        fn remove(&mut self, entity_id: ID) -> Option<T> {
+            let idx_to_page = Self::idx_to_page(entity_id);
+            let idx_into_page = Self::idx_into_page(entity_id);
+
+            // We will swap the last element of the packed arrays with the removed element. Get the packed_idx and the
+            // entity_id of that last element
+            let (packed_idx_of_last, entity_id_of_last) = {
+                let len = self.packed.len();
+                if len > 0 {
+                    (len - 1, self.packed[len - 1])
+                } else {
+                    // We have no component at all
+                    return None;
+                }
+            };
+
+            // Get a mut ref to the page. If the array is too small for the page to be there, then this entity does not
+            // have a component in this pool and we can return.
+            let mut page = self.sparse.get_mut(idx_to_page)?;
+
+            // Get a mut ref to the inner array. If the page is `None`, then this entity does not have a component in this
+            // pool and we can return.
+            let page = page.as_mut()?;
+
+            // Index the page to get the index into the packed arrays.
+            // We don't need to check if `idx_into_page` is small enough because it always will. If it was greater or
+            // equal to the length of the page, it would've been placed in the next page.
+            // If the element was `None`, then this entity does not have a component in this pool and we can return.
+            let packed_idx = page[idx_into_page]?;
+
+            // Remove the element from the page, we read the packed index so we don't need that anymore
+            page[idx_into_page] = None;
+
+            // If the page is now empty, delete it
+            if page.iter().all(|e| e.is_none()) {
+                self.sparse[idx_to_page] = None;
+
+                // If the sparse array need not to be so large anymore because the page got deleted, shrink it
+                while matches!(self.sparse.last(), Some(None)) {
+                    self.sparse.pop();
+                }
+            }
+
+            // Adjust the packed index for the last element of the packed arrays since we will be swapping it with the
+            // deleted element
+            {
+                let idx_to_page_of_last = Self::idx_to_page(entity_id_of_last);
+                let idx_into_page_of_last = Self::idx_into_page(entity_id_of_last);
+
+                self.sparse[idx_to_page_of_last].as_mut().unwrap()[idx_into_page_of_last] = Some(packed_idx);
+            }
+
+            // Swap the last element with the packed index in both packed arrays
+            self.packed.swap(packed_idx, packed_idx_of_last);
+            self.components.swap(packed_idx, packed_idx_of_last);
+
+            // Resize both packed arrays
+            self.packed.pop();
+            Some(self.components.pop().unwrap())
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
     }
 }
