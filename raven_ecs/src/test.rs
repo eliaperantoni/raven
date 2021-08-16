385fbaed51d7ec86f9f772f332225e5aa7555d95
Elia Perantoni <perantonielia0@gmail.com>
2021-08-16 17:23:07 +0200

    Removed CompZ

--- raven_ecs/src/test.rs
+++ raven_ecs/src/test.rs
@@ -29,24 +29,8 @@ impl CompY {
     }
 }
 
-#[derive(Clone, PartialEq, Serialize, Deserialize, Debug)]
-pub struct CompZ {
-    pub f: String,
-}
-
-impl CompZ {
-    pub fn new(s: &str) -> CompZ {
-        CompZ {
-            f: s.to_string(),
-        }
-    }
-}
-
 // #[typetag::serde]
 impl Component for CompX {}
 
 // #[typetag::serde]
 impl Component for CompY {}
-
-// #[typetag::serde]
-impl Component for CompZ {}
