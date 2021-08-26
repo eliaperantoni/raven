bfeee3ffe038c2fe03e06d4b794c865886565be8
Elia Perantoni <perantonielia0@gmail.com>
2021-08-26 17:20:13 +0200

    Downstream scenes

--- raven_core/src/lib.rs
+++ raven_core/src/lib.rs
@@ -5,13 +5,13 @@ use std::path::{Path, PathBuf};
 
 use gl;
 pub use glam;
-pub use mat4;
 use glam::{Mat4, Quat, Vec3};
+pub use mat4;
 use mat4::decompose;
 
 use ecs::*;
 
-use crate::component::{CameraComponent, HierarchyComponent, MeshComponent, TransformComponent};
+use crate::component::{CameraComponent, HierarchyComponent, MeshComponent, SceneComponent, TransformComponent};
 use crate::io::Serializable;
 use crate::resource::{Material, Mesh, Scene};
 use crate::shader::Shader;
@@ -34,57 +34,51 @@ mod standard_shader;
 type Result<T> = ::std::result::Result<T, Box<dyn Error>>;
 
 pub struct Processor {
-    project_root: PathBuf,
-
+    state: State,
     scene: Option<Scene>,
+}
 
+struct State {
+    project_root: PathBuf,
     canvas_size: [u32; 2],
     shader: Shader,
+    camera_mats: Option<CameraMats>,
+}
+
+struct CameraMats {
+    view_mat: Mat4,
+    projection_mat: Mat4,
 }
 
 impl Processor {
     pub fn new<R: AsRef<Path>>(project_root: R) -> Result<Processor> {
         Ok(Processor {
-            project_root: project_root.as_ref().to_owned(),
-
+            state: State {
+                project_root: project_root.as_ref().to_owned(),
+                canvas_size: [800, 400],
+                shader: get_standard_shader()?,
+                camera_mats: None,
+            },
             scene: None,
-
-            canvas_size: [800, 400],
-            shader: get_standard_shader()?,
         })
     }
 
     pub fn load_scene<P: AsRef<Path>>(&mut self, scene_path: P) -> Result<()> {
-        let scene_path = path::as_fs_abs(&self.project_root, scene_path);
+        let scene_path = path::as_fs_abs(&self.state.project_root, scene_path);
 
         self.scene = Some(Scene::load(scene_path)?);
         Ok(())
     }
 
     pub fn set_canvas_size(&mut self, width: u32, height: u32) {
-        self.canvas_size = [width, height];
-    }
-
-    fn clear_canvas(&self) {
-        unsafe {
-            gl::ClearColor(0.1, 0.1, 0.1, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-    }
-
-    fn must_scene(&self) -> &Scene {
-        self.scene.as_ref().expect("no scene loaded")
-    }
-
-    fn must_scene_mut(&mut self) -> &mut Scene {
-        self.scene.as_mut().expect("no scene loaded")
+        self.state.canvas_size = [width, height];
     }
 
     pub fn do_frame(&mut self) -> Result<()> {
-        self.clear_canvas();
+        clear_canvas();
 
         {
-            let [width, height] = self.canvas_size;
+            let [width, height] = self.state.canvas_size;
             unsafe {
                 gl::Viewport(0, 0, width as _, height as _);
             }
@@ -94,41 +88,49 @@ impl Processor {
             return Ok(());
         }
 
-        // First of all, we need to initialize a VAO for each MeshComponent that we haven't seen yet
-        {
-            let project_root = self.project_root.clone();
+        self.state.camera_mats = Some(self.compute_camera_mats().ok_or_else(|| Box::<dyn Error>::from("no camera"))?);
 
-            for (_, (mut mesh_comp, ), _)
-            in <(MeshComponent, )>::query_deep_mut(self.must_scene_mut()) {
-                if mesh_comp.vao.is_some() { continue; };
+        Processor::process_scene(self.scene.as_mut().unwrap(), &mut self.state)?;
 
-                let mesh = Mesh::load(path::as_fs_abs(&project_root, &mesh_comp.mesh))?;
-                let mat = Material::load(path::as_fs_abs(&project_root, &mesh_comp.mat))?;
+        Ok(())
+    }
 
-                let vao = Vao::from(&mesh, &mat)?;
-
-                mesh_comp.vao = Some(vao);
+    fn process_scene(scene: &mut Scene, state: &mut State) -> Result<()> {
+        for (_, (mut scene_comp, ), _)
+        in <(SceneComponent, )>::query_shallow_mut(scene) {
+            if scene_comp.loaded.is_none() {
+                scene_comp.loaded = Some(Scene::load(path::as_fs_abs(&state.project_root, &scene_comp.scene))?);
             }
+
+            Processor::process_scene(scene_comp.loaded.as_mut().unwrap(), state)?;
         }
 
-        let CameraMats { view_mat, projection_mat } = if let Some(mats) = self.compute_camera_mats() {
-            mats
-        } else {
-            return Err(Box::from("no camera"));
-        };
+        for (_, (mut mesh_comp, ), _)
+        in <(MeshComponent, )>::query_deep_mut(scene) {
+            if mesh_comp.vao.is_some() { continue; };
+
+            let mesh = Mesh::load(path::as_fs_abs(&state.project_root, &mesh_comp.mesh))?;
+            let mat = Material::load(path::as_fs_abs(&state.project_root, &mesh_comp.mat))?;
+
+            let vao = Vao::from(&mesh, &mat)?;
+
+            mesh_comp.vao = Some(vao);
+        }
 
         // Now we can properly render them
         for (entity, (mesh_comp, ), _)
-        in <(MeshComponent, )>::query_deep(self.must_scene()) {
-            self.clear_canvas();
-
+        in <(MeshComponent, )>::query_deep(scene) {
             let vao = mesh_comp.vao.as_ref().unwrap();
 
-            self.shader.enable();
-            self.shader.set_mat4("model", &self.combined_transform(entity));
+            state.shader.enable();
+            // TODO Currently only applies transform of this scene. `process_scene` should recursively carry over the
+            //      transform from the parent scene
+            state.shader.set_mat4("model", &combined_transform(scene, entity));
 
-            self.shader.set_mat4("view", &view_mat);
-            self.shader.set_mat4("projection", &projection_mat);
+            let CameraMats{view_mat, projection_mat} = state.camera_mats.as_ref().unwrap();
+
+            state.shader.set_mat4("view", view_mat);
+            state.shader.set_mat4("projection", projection_mat);
 
             vao.draw();
         }
@@ -136,43 +138,12 @@ impl Processor {
         Ok(())
     }
 
-    fn combined_transform(&self, mut entity: Entity) -> Mat4 {
-        let mut transform_components = Vec::new();
-
-        loop {
-            let transform_component = self.must_scene().get_one::<TransformComponent>(entity)
-                .expect("entity does not have a transform component");
-            transform_components.push(transform_component);
-
-            let hierarchy_component = self.must_scene().get_one::<HierarchyComponent>(entity)
-                .expect("entity does not have a hierarchy component");
-
-            if let Some(parent_entity) = hierarchy_component.parent {
-                entity = parent_entity;
-            } else {
-                break;
-            }
-        }
-
-        let mut out = Mat4::IDENTITY;
-
-        for transform_component in transform_components.into_iter() {
-            out = out * transform_component.0;
-        }
-
-        out
-    }
-}
-
-struct CameraMats {
-    view_mat: Mat4,
-    projection_mat: Mat4,
-}
-
-impl Processor {
     fn compute_camera_mats(&self) -> Option<CameraMats> {
-        <(CameraComponent, )>::query_shallow(self.must_scene()).next().map(|(entity, _, _)| {
-            let transform = self.combined_transform(entity);
+        let scene = self.scene.as_ref().unwrap();
+
+        // TODO Search for cameras in downstream scenes
+        <(CameraComponent, )>::query_shallow(scene).next().map(|(entity, _, _)| {
+            let transform = combined_transform(scene, entity);
 
             CameraMats {
                 view_mat: {
@@ -191,11 +162,45 @@ impl Processor {
                     Mat4::look_at_rh(position, target, up)
                 },
                 projection_mat: {
-                    let [width, height] = self.canvas_size;
+                    let [width, height] = self.state.canvas_size;
                     let aspect_ratio = width as f32 / height as f32;
                     Mat4::perspective_rh_gl(90_f32.to_radians(), aspect_ratio, 0.1, 100.0)
                 },
             }
         })
+    }
+}
+
+fn combined_transform(scene: &Scene, mut entity: Entity) -> Mat4 {
+    let mut transform_components = Vec::new();
+
+    loop {
+        let transform_component = scene.get_one::<TransformComponent>(entity)
+            .expect("entity does not have a transform component");
+        transform_components.push(transform_component);
+
+        let hierarchy_component = scene.get_one::<HierarchyComponent>(entity)
+            .expect("entity does not have a hierarchy component");
+
+        if let Some(parent_entity) = hierarchy_component.parent {
+            entity = parent_entity;
+        } else {
+            break;
+        }
+    }
+
+    let mut out = Mat4::IDENTITY;
+
+    for transform_component in transform_components.into_iter() {
+        out = out * transform_component.0;
+    }
+
+    out
+}
+
+fn clear_canvas() {
+    unsafe {
+        gl::ClearColor(0.1, 0.1, 0.1, 1.0);
+        gl::Clear(gl::COLOR_BUFFER_BIT);
     }
 }
